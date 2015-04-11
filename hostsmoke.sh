ee55cd8a8e83be5b6986f899e8b283f880e8f46e
maxigas <maxigas@anargeek.net>
2015-04-11 19:44:00 +0200

    write header file and simplify greps

--- hostsmoke.sh
+++ hostsmoke.sh
@@ -2,4 +2,13 @@
 # Write smokeping's Target file based on what we find in /etc/hosts, and leave a backup of the original Target file.
 set -e
 cp /etc/smokeping/config.d/Targets /etc/smokeping/config.d/Targets`date +%Y-%m-%d`
-grep -v '#' /etc/hosts|grep -vE '^$'|grep -v '::'|grep -v 'localhost'|awk '{print "++" $2 "\n title =" $2 "\n host =" $1 "\n"}'> /etc/smokeping/config.d/Targets
+if [ -f /etc/smokeping/config.d/Header ];
+then
+    cp /etc/smokeping/config.d/Header /etc/smokeping/config.d/Targets
+else
+    echo 'You have to write the header of the target file in:
+/etc/smokeping/config.d/Header'
+    exit 1
+fi
+#grep -v '#' /etc/hosts|grep -vE '^$'|grep -v '::'|grep -v 'localhost'|awk '{print "++" $2 "\n title =" $2 "\n host =" $1 "\n"}'> /etc/smokeping/config.d/Targets
+egrep -v '#|^$|::|localhost'|awk '{print "++" $2 "\n title =" $2 "\n host =" $1 "\n"}'>> /etc/smokeping/config.d/Targets
