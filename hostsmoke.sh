0ea3593244e81b9996d1a892b3d7df23a1d9582a
maxigas <maxigas@anargeek.net>
2015-04-12 04:07:46 +0200

    comment better

--- hostsmoke.sh
+++ hostsmoke.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# Write smokeping's Target file based on what we find in /etc/hosts, and leave a backup of the original Target file.
+# Write smokeping's Target file based on /etc/hosts, leave a backup of the original Target file.
 set -e
+# Backup
 cp /etc/smokeping/config.d/Targets /etc/smokeping/config.d/Targets`date +%Y-%m-%d`
+# Write header
 if [ -f /etc/smokeping/config.d/Header ];
 then
     cp /etc/smokeping/config.d/Header /etc/smokeping/config.d/Targets
-else
+else; 
     echo 'You have to write the header of the target file in:
 /etc/smokeping/config.d/Header'
     exit 1
 fi
-egrep -v '#|^$|::|localhost' /etc/hosts |awk '{print "++" $2 "\n title = " $2 "\n host = " $1 "\n"}'>> /etc/smokeping/config.d/Targets
+# Throw away comments, IPv6 and localhost entries and write Targets file
+egrep -v '#|^$|::|localhost' /etc/hosts | awk '{print "++" $2 "\n title = " $2 "\n host = " $1 "\n"}'>> /etc/smokeping/config.d/Targets
 # Avoid invalid smokeping syntax: remove dots from section names
 sed -i 's/\(++.*\)\.\(.*\)/\1\2/g' /etc/smokeping/config.d/Targets
